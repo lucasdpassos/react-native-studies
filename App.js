2db3838302b917567e033acc497cde1ab2a67a00
Lucas Passos <lucas.depassos@yahoo.com.br>
2020-11-26 16:20:21 -0300

    first_commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Text } from 'react-native'
+
+export default class Estudos extends Component {
+
+  render() {
+
+    return (
+
+      <Text>Olá Mundo222</Text>
+
+    )
+
+  }
+
+}
